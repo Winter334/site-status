19357223f4f3b75ee1bff1d155d2fcf01d182957
流萤白沙 <93890057+Winter334@users.noreply.github.com>
2025-03-19 15:11:15 +0800

    Update SiteFooter.vue

--- app/components/SiteFooter.vue
+++ app/components/SiteFooter.vue
@@ -31,8 +31,8 @@
         {{ $t("footer.fiveMinutes") }}
       </n-p>
       <n-p depth="3">
-        Copyright &copy; 2020 - {{ new Date().getFullYear() }}
-        <n-text depth="3" @click="jumpLink(linkData.home)"> IMSYY </n-text>
+        Copyright &copy; 2024 - {{ new Date().getFullYear() }}
+        <n-text depth="3" @click="jumpLink(linkData.home)"> 流萤白沙 </n-text>
         <n-text
           v-if="siteIcp"
           depth="3"
@@ -51,8 +51,8 @@ const { siteIcp, version } = configPublic;
 
 const linkData = {
   github: "https://github.com/imsyy/site-status",
-  home: "https://www.imsyy.top",
-  email: "mailto:one@imsyy.top",
+  home: "https://lyrashore.com",
+  email: "mailto:ws@lyrashore.com",
 };
 </script>
 
